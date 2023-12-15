963f3616ab4d2e850ff4d2b55a9ff09a05ba8fd3
Vitor Da Silva <vitor.pintodasilva@heig-vd.ch>
2023-12-15 14:59:44 +0100

    git pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://vitpinto.github.io',
+	base: '/labveiltech',
 	integrations: [mdx(), sitemap()],
 });
